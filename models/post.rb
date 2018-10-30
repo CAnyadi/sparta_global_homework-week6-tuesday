973a178b3fc5cc0a8f52efcdb9e0255e901046fb
Chinenye Anyadi <canyadi@spartaglobal.com>
2018-10-30 18:23:55 +0000

    attempted the index page but there's and error

--- models/post.rb
+++ models/post.rb
@@ -3,3 +3,13 @@ class Post
   attr_accessor :title, :author, :description
 
   def self.open_connection
+    conn =PG.connect(dbname: "books", user: "postgres", password: "Acad3my1")
+  end
+
+#CONN = Connect
+  def self.all
+    conn = self.open_connection
+    sql = "SELECT * FROM books ORDER BY id"
+    results = conn.exec(sql)
+  end
+end
